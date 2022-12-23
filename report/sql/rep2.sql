6346e767bbb738624f2e6cc994db28a60a9409a9
nomilious <eduard.belic.2002@gmail.com>
2022-12-24 00:05:16 +0300

    Added sql_dump.sql

--- report/sql/rep2.sql
+++ report/sql/rep2.sql
@@ -1,9 +1,9 @@
 SELECT
 	eq_id,
     title,
-    protocol_id,
     status
 FROM REPORTS_EQUIPMENT
 	join REPORT_DATES on report_date_id = REPORT_DATES.id
 where year = "$in_year"
 	and month = "$in_month"
+order by eq_id
